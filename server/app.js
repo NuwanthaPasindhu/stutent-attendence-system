7ffae5411428c209e1f536ad39ea2cf5dd1a64d6
NuwanthaPasindhu <hnpkdias@gmail.com>
2022-11-11 17:22:25 +0530

    initial commit with directories

---
+++ server/app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+const authRoutes = require("./routes/auth");
+const passport = require("passport");
+
+
+// DB CONNECTION
+const dbConnection = require('./config/db.config');
+
+
+// DECLARING EXPRESS SESSION
+const app = express();
+
+// DEFINE CORS
+app.use(cors());
+
+
+//DEFINE EXPRESS JSON
+app.use(express.json());
+
+// ASSIGN THE PORT 
+const PORT = process.env.PORT || 4000;
+
+// CREATING AN EXPRESS SERVER
+app.listen(PORT, () => {
+    console.log(`Server up and run in port ${PORT}`)
+})
